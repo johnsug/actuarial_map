e6d10a8233519b16ca1c70689c7bd33a819236be
johnsug <john.sugden@cerner.com>
2016-01-23 21:41:19 -0600

    Update map_app.R

--- map_app.R
+++ map_app.R
@@ -15,11 +15,15 @@ server <- function(input, output){
   d <- read.csv("C:/Users/JS033085/Desktop/EQ/MO.csv")
   
   ## shape
+  names(d)[1] <- "Practice"
   names(d)[6] <- "State"
-  d <- sqldf("select City, State, Latitude, Longitude, count(City) as Count from d group by 1, 2, 3, 4")
+  d$Practice[d$Practice %in% c("Accounting and Finance", "Investment / ALM", "Risk Management")] <- "Life"
+  d$Practice <- droplevels(d$Practice)
+  d <- sqldf("select Practice, Latitude, Longitude, count(Practice) as Count from d group by 1, 2, 3")
   
   output$map <- renderLeaflet({
-    leaflet(d) %>% addTiles() %>% addCircles(~Longitude, ~Latitude, radius=~Count^2*10, fillOpacity=.3, weight=1) %>% 
+    leaflet(d) %>% addTiles() %>% addCircles(~Longitude, ~Latitude, group=~Practice, radius=~Count*1000, 
+    fillOpacity=.3, weight=1) %>% 
       addProviderTiles("CartoDB.Positron") %>% addLayersControl(baseGroups=c("Health", "Life", "P&C", "Retirement"), 
                                                                 options=layersControlOptions(collapsed=FALSE))
   })
